783aab382049d18d14ee92431fe3480a06affa33
Ashtam <ashtam@artiosys.com>
2022-03-09 15:40:40 +0530

    connected third page using promises

---
+++ cart-ui/second.js
@@ -0,0 +1,64 @@
+//Validate User
+//Fetching user wallet details
+//send user to cart page
+
+// var text = document.getElementById('loadingText')
+// var walletBal = document.getElementById('walletBalance')
+
+// function validateUser(){
+//     text.innerHTML = "Validating User ...";
+//     setTimeout(function() {
+//         text.innerHTML = "User Validation Done.";
+//         }, 1000);
+// }
+
+
+// function fetchWallet(){
+//     text.innerHTML = "Fetching Wallet Balance ...";
+//     setTimeout(function() {
+//         walletBal.innerHTML = "500";
+//         }, 1000);
+// }
+
+
+
+// const button = document.querySelector("button");
+const div = document.getElementById("loadingText");
+
+const setText = (text) => {
+  div.textContent = text
+}
+
+const checkAuth = () => {
+  return new Promise((resolve, reject) => {
+    setText('Checking Auth...')
+    setTimeout(() => {
+      resolve(true);
+    }, 2000);
+  });
+};
+
+const fetchUser = () => {
+  return new Promise((resolve, reject) => {
+    setText('Fetching User...');
+    setTimeout(() => {
+      resolve({ name: "Max" });
+    }, 2000);
+  });
+};
+
+// button.addEventListener("click", () => {
+  checkAuth()
+     .then(
+        isAuth => {
+          if (isAuth) {
+            return fetchUser()
+          }
+        }
+      )
+      .then(
+        user => {
+            window.location.href = "cart.html";
+        }
+      );
+// });
