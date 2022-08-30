725c457d21fce6ff3c198ffebf813223de93f696
Ashtam singh <ashtam.delhi@gmail.com>
2022-08-30 17:07:18 +0530

    init

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <?php include_once("index.html"); ?>
+<?php if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; } ?>
