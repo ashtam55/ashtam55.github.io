131c0cb260aae971af2ce969f221618992d105bf
Ashtam singh <ashtam.delhi@gmail.com>
2022-08-30 17:13:22 +0530

    init

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php include_once("index.html"); ?>
-<?php if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; } ?>
+<?php include_once("index.html");
+ if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){ $protocol = "https://".$_SERVER['HTTP_HOST']; } else{ $protocol='http://'.$_SERVER['HTTP_HOST']; } ?>
